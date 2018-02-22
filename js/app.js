912eee1e9e800eff64663f693cb752310a50342d
Chris Hamm <chris.m.hamm@gmail.com>
2018-02-22 12:11:53 -0500

    reorganize and make more items variables

--- js/app.js
+++ js/app.js
@@ -1,39 +1,74 @@
-//set global vars
+// Set Global Variables - Do not change these
 var canvas;
 var canvasContext;
+var winMusicHasPlayed = false;
+var loseMusicHasPlayed = false;
+var player1Score = 0;
+var player2Score = 0;
+var showingWinScreen = false;
 
+// Color Variables
+/*
+  see http://www.color-hex.com/ for more options
+*/
+var baseColor= 'white';
 var canvasColor = '#5e871f';
 var ballColor = 'white';
 var paddle1Color = '#C9F227';
 var paddle2Color = '#00F8FB';
+var netColor = 'white';
 
-var paddleSound = new Audio('sounds/bounce.mp3');
-var bgMusic = new Audio('sounds/music.mp3');
-var playerScoreSound = new Audio('sounds/score.mp3');
-var computerScoreSound = new Audio('sounds/computer-score.mp3');
-var winMusic = new Audio('sounds/win.mp3');
-var loseMusic = new Audio('sounds/fail.mp3');
-var winMusicHasPlayed = false;
-var loseMusicHasPlayed = false;
+// Sound Variables
+/*
+  Options:
+  - sounds/blue.mp3
+  - sounds/bounce.mp3
+  - sounds/computer-score.mp3
+  - sounds/fail.mp3
+  - sounds/guitar.wav
+  - sounds/lose.mp3
+  - sounds/music.mp3
+  - sounds/score.mp3
+  - sounds/win.mp3
+*/
+const paddleSound = new Audio('sounds/bounce.mp3');
+const bgMusic = new Audio('sounds/music.mp3');
+const playerScoreSound = new Audio('sounds/score.mp3');
+const computerScoreSound = new Audio('sounds/computer-score.mp3');
+const winMusic = new Audio('sounds/win.mp3');
+const loseMusic = new Audio('sounds/fail.mp3');
 
-var player1Score = 0;
-var player2Score = 0;
+// Gameplay Variables
 const WINNING_SCORE = 3;
+const FPS = 30;
+var aiDifficulty = 8;
+var winMessage = 'GG, bruh';
+var loseMessage = 'that was kind of lame...';
+var playButtonText = 'play again';
 
-var showingWinScreen = false;
+// Net Variables
+const NET_WIDTH = 5;
+const NET_LINE_HEIGHT = 30;
+const NET_LINE_SPACING = 60;
 
-var ballX = 50;
-var ballY = 50;
-var ballSpeedX = 10;
-var ballSpeedY = 4;
-var ballSize = 10;
+// Ball Variables
+var ballX = 50; // Ball's starting X position
+var ballY = 50; // Ball's starting Y position
+var ballSpeedX = 10; // Ball's speed along the X axis
+var ballSpeedY = 4; // Ball's speed along the Y axis
+var ballSize = 10; // Ball's diameter in pixels
 
+// Paddle Variables
 const PADDLE_WIDTH = 10;
 const PADDLE_HEIGHT = 100;
 const PADDLE_SPACE_FROM_SIDE = 15;
-var paddle1Y = 200;
-var paddle2Y = 200;
+var paddle1Y = 200; // Paddle's starting Y position
+var paddle2Y = 200; // Paddle's starting Y position
 
+
+// Functions
+
+// Restart everything when you click the Play Again button
 function handleMouseClick(evt) {
   if(showingWinScreen) {
     player1Score=0;
@@ -50,28 +85,31 @@ function handleMouseClick(evt) {
   }
 }
 
-//run on window load
+// Run functions after the window has loaded
 window.onload = function() {
+
+  // Get the canvas element
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
   
-  //set fps and init drawing
-  var fps = 30;
+  // Set the FPS, initialize movement and drawing
   setInterval(function() {
     moveEverything();
     drawEverything();
-  }, 1000/fps);
+  }, 1000/FPS);
 
 
-
+  // Start music
   bgMusic.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
   }, false);
   bgMusic.play();
 
+  // Restart the game when you click
   canvas.addEventListener('mousedown', handleMouseClick);
 
+  // Make the player paddle follow the mouse
   canvas.addEventListener('mousemove', function(evt){
     var mousePos = calculateMousePos(evt);
     paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
@@ -79,18 +117,19 @@ window.onload = function() {
   });
 }
 
+// Handle computer AI
 function computerMovement() {
   var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
 
   if (paddle2YCenter < ballY-35) {
-    paddle2Y += 8;
+    paddle2Y += aiDifficulty;
   }
   else if (paddle2YCenter > ballY+35) {
-    paddle2Y -= 8;
+    paddle2Y -= aiDifficulty;
   }
 }
 
-//animation
+// Animation and movement
 function moveEverything() {
   if(showingWinScreen) {
     return;
@@ -101,7 +140,7 @@ function moveEverything() {
   ballX += ballSpeedX;
   ballY += ballSpeedY;
 
-  //left side bounce or reset 
+  // Bounce or reset for the player side of the screen 
   if(ballX < PADDLE_SPACE_FROM_SIDE + PADDLE_WIDTH + ballSize) {
     if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
       paddleSound.play();
@@ -119,7 +158,8 @@ function moveEverything() {
       ballReset();
     }
   }
-  //right side bounce or reset
+
+  // Bounce or reset for the computer side of the screen
   if(ballX > canvas.width - PADDLE_SPACE_FROM_SIDE - PADDLE_WIDTH - ballSize) {
     if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
       paddleSound.play();
@@ -138,13 +178,14 @@ function moveEverything() {
     }
     
   }
-  //bounce on y axis
+
+  // Bounce off top and bottom of screen
   if(ballY > canvas.height - ballSize || ballY < ballSize) {
     ballSpeedY = -ballSpeedY;
   }
 }
 
-// reset ball
+// Reset the ball after a score
 function ballReset() {
   if (player1Score >= WINNING_SCORE || player2Score >= WINNING_SCORE) {
     showingWinScreen = true;
@@ -155,19 +196,20 @@ function ballReset() {
   ballY = canvas.height/2;
 }
 
-//draw
+// Draw the net
 function drawNet() {
-  for (var i=0; i<canvas.height; i+=40) {
-    drawRect(canvas.width/2-1, i, 2, 20, 'white');
+  for (var i=0; i<canvas.height; i+=NET_LINE_SPACING) {
+    drawRect(canvas.width/2-1, i, NET_WIDTH, NET_LINE_HEIGHT, netColor);
   }
 }
 
+// Draw everything
 function drawEverything() {
 
-  //draw canvas
+  // Draw the playing field
   drawRect(0, 0, canvas.width, canvas.height, canvasColor);
 
-  canvasContext.fillStyle = 'white';
+  canvasContext.fillStyle = baseColor;
 
   if(showingWinScreen) {
     if (player1Score >= WINNING_SCORE) {
@@ -177,7 +219,8 @@ function drawEverything() {
         winMusic.play();
         winMusicHasPlayed = true;
       }
-      canvasContext.fillText('You Won!!!',300,200);
+      canvasContext.fillText("Player Won!",200,200);
+      canvasContext.fillText(winMessage,200,300);
     }
     else if(player2Score >= WINNING_SCORE) {
       bgMusic.pause();
@@ -187,21 +230,22 @@ function drawEverything() {
         loseMusicHasPlayed = true;
       }
       canvasContext.fillText("Computer Won...",200,200);
-      canvasContext.fillText("you're kind of lame",200,250);
+      canvasContext.fillText(loseMessage,200,300);
     }
 
     
-    canvasContext.fillText('play again',300,500);
+    canvasContext.fillText(playButtonText,300,500);
     return;
   }
 
   drawNet();
 
-  //draw user paddle
+  // Draw user paddle
   drawRect(PADDLE_SPACE_FROM_SIDE, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT, paddle1Color);
-  //draw computer paddle 
+
+  // Draw computer paddle 
   drawRect((canvas.width - PADDLE_WIDTH)-PADDLE_SPACE_FROM_SIDE, paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT, paddle2Color)
-  //draw ball
+  // Draw ball
   drawBall(ballX, ballY, ballSize, ballColor);
 
   canvasContext.font = "50px Arial";
@@ -209,13 +253,13 @@ function drawEverything() {
   canvasContext.fillText(player2Score,canvas.width-200,100);
 }
 
-// draw rectangles
+// Function for drawing rectangles
 function drawRect(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.fillRect(leftX,topY,width,height);
 }
 
-// draw ball
+// Function for drawing balls
 function drawBall(leftX, topY, size, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.beginPath();
@@ -223,7 +267,7 @@ function drawBall(leftX, topY, size, drawColor) {
   canvasContext.fill();
 }
 
-// player controls
+// Calculate where the mouse is
 function calculateMousePos(evt) {
   var rect = canvas.getBoundingClientRect();
   var root = document.documentElement;
